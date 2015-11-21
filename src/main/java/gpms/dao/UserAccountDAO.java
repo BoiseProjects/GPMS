cf13f791338d85c7c40ba08e48137c8ffa2b009b
milstein <milsonmun@yahoo.com>
2015-11-21 13:44:06 -0700

    Added UserProfile classes and with all dependencies closed #25

--- src/main/java/gpms/dao/UserAccountDAO.java
+++ src/main/java/gpms/dao/UserAccountDAO.java
@@ -1,6 +1,8 @@
 package gpms.dao;
 
 import gpms.DAL.MongoDBConnector;
+import gpms.model.AuditLog;
+import gpms.model.GPMSCommonInfo;
 import gpms.model.UserAccount;
 import gpms.model.UserProfile;
 
@@ -48,100 +50,5 @@ public class UserAccountDAO extends BasicDAO<UserAccount, String> {
 
 	public UserAccountDAO(MongoClient mongo, Morphia morphia, String dbName) {
 		super(mongo, morphia, dbName);
-	}
-
-	public List<UserAccount> findAll() throws UnknownHostException {
-		// if decision permit = 0
-		// do
-		// else don't
-		Datastore ds = getDatastore();
-		return ds.createQuery(UserAccount.class).asList();
-	}
-
-	public UserAccount findByID(ObjectId id) {
-		Datastore ds = getDatastore();
-		return ds.createQuery(UserAccount.class).field("_id").equal(id).get();
-	}
-
-	/**
-	 * This method will return a user account object that matches the username
-	 * searched for
-	 * 
-	 * @param userName
-	 *            the user account name to search for
-	 * @return the username if it exists or null if it does not
-	 */
-	public UserAccount findByUserName(String userName) {
-		Datastore ds = getDatastore();
-		return ds.createQuery(UserAccount.class).field("username")
-				.equal(userName).get();
-	}
-
-	/**
-	 * Method to set account name
-	 * 
-	 * @param author
-	 *            profile making the change
-	 * @param target
-	 *            profile being changed
-	 * @param newName
-	 *            the new name for the user
-	 */
-	public void setAccountName(UserProfile author, UserAccount target,
-			String newName) {
-		Datastore ds = getDatastore();
-		if (!target.getUserName().equals(newName)) {
-			audit = new AuditLog(author, "Edited user name", new Date());
-			target.addEntryToAuditLog(audit);
-			target.setUserName(newName);
-			ds.save(target);
-		}
-	}
-
-	/**
-	 * Method to set the password
-	 * 
-	 * @param author
-	 *            profile making the change
-	 * @param target
-	 *            profile the change will be made to
-	 * @param newPassword
-	 *            the new password
-	 */
-	public void setPassword(UserProfile author, UserAccount target,
-			String newPassword) {
-		Datastore ds = getDatastore();
-		if (!target.getPassword().equals(newPassword)) {
-			audit = new AuditLog(author, "Edited password", new Date());
-			target.addEntryToAuditLog(audit);
-			target.setPassword(newPassword);
-			ds.save(target);
-		}
-	}
-
-	public void deleteUserAccountByUserID(UserAccount userAccount,
-			UserProfile authorProfile, GPMSCommonInfo gpmsCommonObj) {
-		Datastore ds = getDatastore();
-		audit = new AuditLog(authorProfile, "Deleted user account for "
-				+ userAccount.getUserName(), new Date());
-		userAccount.addEntryToAuditLog(audit);
-
-		userAccount.setDeleted(true);
-		userAccount.setActive(false);
-		ds.save(userAccount);
-	}
-
-	public void activateUserAccountByUserID(UserAccount userAccount,
-			UserProfile authorProfile, GPMSCommonInfo gpmsCommonObj,
-			Boolean isActive) {
-		Datastore ds = getDatastore();
-		audit = new AuditLog(authorProfile, "Activated user account for "
-				+ userAccount.getUserName(), new Date());
-		userAccount.addEntryToAuditLog(audit);
-
-		userAccount.setDeleted(!isActive);
-		userAccount.setActive(isActive);
-		ds.save(userAccount);
-
-	}
+	}	
 }
