0aa769693f8c5a6919087629180deeffd6587204
thomasvolz <thomasvolz@u.boisestate.edu>
2015-11-24 12:57:01 -0700

    closes #43

---
+++ src/main/java/gpms/rest/ProposalService.java
@@ -0,0 +1,100 @@
+package gpms.rest;
+
+import gpms.DAL.MongoDBConnector;
+import gpms.accesscontrol.Accesscontrol;
+import gpms.dao.ProposalDAO;
+import gpms.dao.UserAccountDAO;
+import gpms.dao.UserProfileDAO;
+import gpms.model.AdditionalInfo;
+import gpms.model.AuditLogInfo;
+import gpms.model.BaseInfo;
+import gpms.model.BaseOptions;
+import gpms.model.BasePIEligibilityOptions;
+import gpms.model.CollaborationInfo;
+import gpms.model.ComplianceInfo;
+import gpms.model.ConfidentialInfo;
+import gpms.model.ConflictOfInterest;
+import gpms.model.CostShareInfo;
+import gpms.model.FundingSource;
+import gpms.model.GPMSCommonInfo;
+import gpms.model.InvestigatorInfo;
+import gpms.model.InvestigatorRefAndPosition;
+import gpms.model.OSPSectionInfo;
+import gpms.model.ProjectInfo;
+import gpms.model.ProjectLocation;
+import gpms.model.ProjectPeriod;
+import gpms.model.ProjectType;
+import gpms.model.Proposal;
+import gpms.model.ProposalInfo;
+import gpms.model.Recovery;
+import gpms.model.ResearchAdministrator;
+import gpms.model.SignatureInfo;
+import gpms.model.SponsorAndBudgetInfo;
+import gpms.model.Status;
+import gpms.model.TypeOfRequest;
+import gpms.model.UniversityCommitments;
+import gpms.model.UserAccount;
+import gpms.model.UserProfile;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.bson.types.ObjectId;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.mongodb.morphia.Morphia;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mongodb.MongoClient;
+
+@Path("/proposals")
+@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+public class ProposalService {
+	MongoClient mongoClient = null;
+	Morphia morphia = null;
+	String dbName = "db_gpms";
+	UserAccountDAO userAccountDAO = null;
+	UserProfileDAO userProfileDAO = null;
+	ProposalDAO proposalDAO = null;
+
+	DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+	public ProposalService() {
+		mongoClient = MongoDBConnector.getMongo();
+		morphia = new Morphia();
+		morphia.map(UserProfile.class).map(UserAccount.class);
+		userAccountDAO = new UserAccountDAO(mongoClient, morphia, dbName);
+		userProfileDAO = new UserProfileDAO(mongoClient, morphia, dbName);
+		proposalDAO = new ProposalDAO(mongoClient, morphia, dbName);
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String returnString() {
+		return "Hello World!";
+	}
+
+
+}
