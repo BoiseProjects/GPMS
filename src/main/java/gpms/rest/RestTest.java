5c4fa635e3841bc2e9348618549a4cc98f9879ca
Milstein Muanakmi <milsonmun@yahoo.com>
2016-02-12 10:44:47 -0700

    testing for download using REST

--- src/main/java/gpms/rest/RestTest.java
+++ src/main/java/gpms/rest/RestTest.java
@@ -16,10 +16,10 @@ import javax.ws.rs.core.Response;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
-@Path("/upload")
+@Path("/file")
 public class RestTest {
 	@POST
-	@Path("/pdf")
+	@Path("/upload")
 	@Consumes({ MediaType.MULTIPART_FORM_DATA })
 	public Response uploadPdfFile(
 			@FormDataParam("file") InputStream fileInputStream,
@@ -41,6 +41,31 @@ public class RestTest {
 			throw new WebApplicationException();
 		}
 		return Response.ok("Data uploaded successfully !!").build();
-
 	}
+
+	@POST
+	@Path("/download")
+	@Consumes({ MediaType.MULTIPART_FORM_DATA })
+	public Response uploadPdfFile(
+			@FormDataParam("file") InputStream fileInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileMetaData)
+			throws Exception {
+		String UPLOAD_PATH = "c:/temp/";
+		try {
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			OutputStream out = new FileOutputStream(new File(UPLOAD_PATH
+					+ fileMetaData.getFileName()));
+			while ((read = fileInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			throw new WebApplicationException();
+		}
+		return Response.ok("Data uploaded successfully !!").build();
+	}
+
 }
